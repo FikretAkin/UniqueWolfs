ed62d4226f6cea15db067ec7c77bcaf4d575222b
Fikret AKIN <info@fikretakin.com>
2023-01-06 07:31:16 +0300

    Minor bug fixed.

--- UniqueWolfs/Program.cs
+++ UniqueWolfs/Program.cs
@@ -4,6 +4,7 @@
 
     var sortedUniqueWolfs = uniqueWolfs
         .GroupBy(x => x)
+        .OrderBy(x => x.Key)
         .OrderByDescending(x => x.Count())
         .Select(x => x.Key);
     int mostsortedUniqueWolf = sortedUniqueWolfs.First();
@@ -24,7 +25,10 @@ Console.WriteLine("List Value İnput Sample:  2 3 4 5 4 3 2 1 3 4");
 #endif
 string TwoValue = Console.ReadLine();
 
-List<int> arrList = TwoValue.Split(' ').Select(n => Convert.ToInt32(n)).ToArray().ToList();
+List<int> arrList = TwoValue.Split(' ')
+    .Select(n => Convert.ToInt32(n))
+    .ToArray()
+    .ToList();
 if (arrList.Exists(x => x > 5) || arrList.Exists(x => x < 1))
 {
     Console.WriteLine("Error Number");
